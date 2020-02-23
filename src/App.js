2e2f4507ed761501dad79135657fab26bee42c24
Juan David Nunez <juandavidnh93@gmail.com>
2020-02-22 22:25:05 -0500

    added Bearer to delete

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ class App extends React.Component {
       method: 'DELETE',
       headers: {
         'content-type': 'application/json',
-        'Authorization': `${credentials.API_KEY}`,
+        'Authorization': `Bearer ${credentials.API_KEY}`,
       },
     })
     
